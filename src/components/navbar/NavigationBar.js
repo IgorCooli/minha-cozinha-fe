5bd2da0bdb1d6db787b0dac6e0095f287eb30027
igorcooli <igorcooli@gmail.com>
2024-06-01 15:25:21 -0300

    rename app

--- src/components/navbar/NavigationBar.js
+++ src/components/navbar/NavigationBar.js
@@ -22,30 +22,6 @@ const NavigationBar = () => {
                         <i className="fa-solid fa-house-chimney"></i>
                     </Button>
                 </div>
-                {/*<Navbar.Toggle aria-controls={`offcanvasNavbar-expand-large`} />*/}
-                {/*<Navbar.Collapse id={`offcanvasNavbar-expand-large`} className="text-end">*/}
-                {/*    <Nav className="justify-content-end flex-grow-1 pe-3">*/}
-                {/*        <Nav.Link style={{ color: '#333333' }} href="/home">*/}
-                {/*            Home*/}
-                {/*        </Nav.Link>*/}
-                {/*        <div style={{ background: 'linear-gradient(100deg, transparent, #333333)', height: '1px' }}></div>*/}
-                {/*        <Nav.Link style={{ color: '#333333' }} href="/register-sale">*/}
-                {/*            Nova venda*/}
-                {/*        </Nav.Link>*/}
-                {/*        <div style={{ background: 'linear-gradient(100deg, transparent, #333333)', height: '1px' }}></div>*/}
-                {/*        <Nav.Link style={{ color: '#333333' }} href="/sales-history">*/}
-                {/*            Histórico de vendas*/}
-                {/*        </Nav.Link>*/}
-                {/*        <div style={{ background: 'linear-gradient(100deg, transparent, #333333)', height: '1px' }}></div>*/}
-                {/*        <Nav.Link style={{ color: '#333333' }} href="/register-customer">*/}
-                {/*            Cadastrar cliente*/}
-                {/*        </Nav.Link>*/}
-                {/*        <div style={{ background: 'linear-gradient(100deg, transparent, #333333)', height: '1px' }}></div>*/}
-                {/*        <Nav.Link style={{ color: '#333333' }} onClick={handleLogout}>*/}
-                {/*            Logout*/}
-                {/*        </Nav.Link>*/}
-                {/*    </Nav>*/}
-                {/*</Navbar.Collapse>*/}
             </Navbar>
         </div>
     );
